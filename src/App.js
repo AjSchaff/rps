64225380914a2207153bdc5bb7d2e28cd9f811f1
Andrew Schaffner <aschaff4@gmail.com>
2019-06-07 18:11:57 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const UserWeapon = props => (
+  <h1>
+    {props.userWeapon} | {props.AIChoice}
+  </h1>
+);
+
+const Outcome = props => <h3>{props.outcome}</h3>;
+
+const Score = props => (
+  <h5>
+    {props.score.wins} | {props.score.loses}
+  </h5>
+);
+
+const Game = () => {
+  const weaponsAvailable = ["rock", "paper", "scissors"];
+  const [weapon, selectingWeapon] = useState({
+    choice: "Choose your weapon",
+    AIChoice: ""
+  });
+  const [outcome, setOutcome] = useState({
+    outcome: "You: "
+  });
+  const score = {
+    wins: 0,
+    loses: 0
+  };
+
+  function testGame(weapon) {
+    debugger;
+    if (weapon.choice === weapon.AIChoice) {
+      setOutcome({ ...outcome, outcome: "You Tied!" });
+    }
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    selectingWeapon({
+      ...weapon,
+      choice: [e.target.value],
+      AIChoice: [weaponsAvailable[Math.floor(Math.random() * 3) + 0]]
+    });
+  }
+
+  return (
+    <div>
+      <UserWeapon userWeapon={weapon.choice} AIChoice={weapon.AIChoice} />
+      {weaponsAvailable.map(item => {
+        return (
+          <button onClick={handleClick} key={item} value={item}>
+            {item}
+          </button>
+        );
+      })}
+      <Outcome outcome={outcome.outcome} />
+      <Score score={score} />
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Rock Paper Scissors
+        <Game />
       </header>
     </div>
   );
